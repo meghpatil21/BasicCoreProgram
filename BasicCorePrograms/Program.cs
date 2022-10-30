b6bb4eeff91651132b89bac621e9fe7e14967222
meghpatil21 <megh.after18@gmail.com>
2022-10-30 20:33:23 +0530

    Implemented Leap Year

--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -6,11 +6,16 @@
         {
             Console.WriteLine("Welcome to Basic Core Programs.");
 
-            Console.WriteLine("Select option \n1.Flip Coin");
+            Console.WriteLine("Select option \n1.Flip Coin \n2.Leap Year");
             int opt = Convert.ToInt32(Console.ReadLine());
             switch(opt)
             {
                 case 1: FlipCoin.CoinFlip();
+                    break;
+                case 2: LeapYear.LeapYr();
+                    break;
+
+                default: Console.WriteLine("Invalid Choice");
                     break;
 
             }
@@ -58,6 +63,37 @@
                     Console.WriteLine("Tail Percent: " + (100 - headPercent));
 
                 }
+            }
+        }
+
+        public class LeapYear
+        {
+            public static void LeapYr()
+            {
+                Console.WriteLine("Enter Year :");
+                string str = Console.ReadLine();
+                int length = str.Length;
+
+                if (length != 4)
+                {
+                    Console.WriteLine("You must enter 4 digit number. Try Again.");
+                }
+                else
+                {
+                    int Year = Convert.ToInt32(str);
+
+                    if ((Year % 4 == 0 && Year % 100 != 0) || (Year % 400 == 0))
+                    {
+                        Console.WriteLine("{0} is leap year.", Year);
+                    }
+                    else
+                    {
+                        Console.WriteLine("{0} is not leap year.", Year);
+                    }
+
+
+                }
+
             }
         }
     }
